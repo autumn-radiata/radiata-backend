008ce722806899da44c22807447b606c9b05c3f3
yunjae62 <wodbs7758@naver.com>
2024-10-01 17:27:28 +0900

    [#3] feat(payment): 결제 관련 VO 추가
    - 계좌
    - Money

---
+++ service/payment/payment-core/src/main/java/radiata/service/payment/core/domain/model/vo/Money.java
@@ -0,0 +1,47 @@
+package radiata.service.payment.core.domain.model.vo;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable // JPA 값객체 어노테이션
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 엔티티, 임베디드는 기본 생성자 필수
+public class Money {
+
+    public static final Money ZERO = new Money(0L);
+    
+    private Long amount;
+
+    public Money(long amount) {
+        this.amount = amount;
+    }
+
+    public static Money of(long amount) {
+        return new Money(amount);
+    }
+
+    public Money add(Money money) {
+        return new Money(this.amount + money.amount);
+    }
+
+    public Money subtract(final Money money) {
+        return new Money(this.amount - money.amount);
+    }
+
+    public Money multiply(int multiplier) {
+        return new Money(this.amount * multiplier);
+    }
+
+    public boolean isNegative() {
+        return amount < 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(amount);
+    }
+}
