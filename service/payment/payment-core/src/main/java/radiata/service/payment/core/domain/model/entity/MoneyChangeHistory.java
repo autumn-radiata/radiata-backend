6fa8f9fd4e54bccfed9244a32124730f6d1b17a3
yunjae62 <wodbs7758@naver.com>
2024-10-01 17:28:34 +0900

    [#3] feat(payment): 충전금 증감 내역 엔티티 추가

---
+++ service/payment/payment-core/src/main/java/radiata/service/payment/core/domain/model/entity/MoneyChangeHistory.java
@@ -0,0 +1,48 @@
+package radiata.service.payment.core.domain.model.entity;
+
+import com.github.ksuid.Ksuid;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+import radiata.service.payment.core.domain.model.vo.Money;
+
+@Getter
+@Entity
+@Table(name = "r_money_change_history")
+@SQLRestriction("deleted_at IS NOT NULL")
+@Builder // 빌더 패턴은 테스트 용도로만 사용
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 객체는 기본 생성자를 필요로 함
+public class MoneyChangeHistory {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private Money amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pay_user_id", nullable = false)
+    private PayUser payUser;
+
+    public static MoneyChangeHistory of(
+        Money amount,
+        PayUser payUser
+    ) {
+        return MoneyChangeHistory.builder()
+            .id(Ksuid.newKsuid().toString())
+            .amount(amount)
+            .payUser(payUser)
+            .build();
+    }
+}
