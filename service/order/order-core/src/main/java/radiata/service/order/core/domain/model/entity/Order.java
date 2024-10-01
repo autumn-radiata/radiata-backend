311c44a02e259cb209c1c3d6d306cb328bd87e04
atto08 <atto08@naver.com>
2024-10-01 22:29:25 +0900

    feat(order): Order, OrderItem 엔티티 생성

---
+++ service/order/order-core/src/main/java/radiata/service/order/core/domain/model/entity/Order.java
@@ -0,0 +1,79 @@
+package radiata.service.order.core.domain.model.entity;
+
+import jakarta.persistence.*;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import radiata.service.order.core.domain.model.vo.OrderStatus;
+
+@Entity
+@Table(name = "r_order")
+@Getter
+@Builder // 테스트 용
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Order {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private Integer orderPrice;
+
+    @Column(nullable = false)
+    private Boolean isRefunded;
+
+    @Column(nullable = false)
+    private String address;
+
+    private String paymentId;
+
+//    private PaymentType paymentType;
+
+    private String comment;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private Set<OrderItem> itemList;
+
+    // 주문 생성
+    public static Order create(
+        String id,
+        String userId,
+        Integer orderPrice,
+        String address,
+        String comment) {
+
+        return Order.builder()
+            .id(id)
+            .userId(userId)
+            .status(OrderStatus.PAYMENT_REQUESTED)
+            .orderPrice(orderPrice)
+            .isRefunded(false)
+            .address(address)
+            .comment(comment)
+            .build();
+    }
+
+    public void setOrderItems(Set<OrderItem> itemList) {
+        this.itemList = itemList;
+    }
+
+    // 주문 상태 변경
+    public void updateOrderStatus(
+        OrderStatus status
+    ) {
+        this.status = status;
+    }
+
+    // 주문 삭제
+}
