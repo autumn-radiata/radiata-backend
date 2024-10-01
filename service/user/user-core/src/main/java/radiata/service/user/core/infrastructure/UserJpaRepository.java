b8478b3e38eda7c2821081076e5eda2b806f877f
SunMi <wywudi@naver.com>
2024-10-01 22:05:11 +0900

    feat(user) : user,point repository 구현체 생성

---
+++ service/user/user-core/src/main/java/radiata/service/user/core/infrastructure/UserJpaRepository.java
@@ -0,0 +1,7 @@
+package radiata.service.user.core.infrastructure;
+
+import radiata.service.user.core.domain.repository.UserRepository;
+
+public interface UserJpaRepository extends UserRepository {
+
+}
