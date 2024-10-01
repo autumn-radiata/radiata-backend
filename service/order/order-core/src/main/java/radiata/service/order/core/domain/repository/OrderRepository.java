cd4bacd39a999e27c105c623975a57753fe49d5e
atto08 <atto08@naver.com>
2024-10-01 22:30:43 +0900

    feat(order): Order 리포지토리 생성

---
+++ service/order/order-core/src/main/java/radiata/service/order/core/domain/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package radiata.service.order.core.domain.repository;
+
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+import radiata.service.order.core.domain.model.entity.Order;
+
+@Repository
+public interface OrderRepository {
+
+    Order save(Order order);
+
+    Optional<Order> findById(String id);
+}
