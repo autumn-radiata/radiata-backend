68d7a9b7f3c28571be5ce95675535056c8788948
atto08 <atto08@naver.com>
2024-10-02 23:22:26 +0900

    [#18] feat(order): 주문 생성 responseDto itemList 추가

--- common/src/main/java/radiata/common/domain/order/dto/response/OrderResponseDto.java
+++ common/src/main/java/radiata/common/domain/order/dto/response/OrderResponseDto.java
@@ -1,14 +1,1 @@
-package radiata.common.domain.order.dto.response;
-
-public record OrderResponseDto(
-    String orderId,
-    String userId,
-    String status,
-    Integer orderPrice,
-    Boolean isRefunded,
-    String address,
-    String paymentId,
-    String comment
-) {
-
-}
+package radiata.common.domain.order.dto.response;import java.util.List;import lombok.AccessLevel;import lombok.Builder;@Builder(access = AccessLevel.PRIVATE)public record OrderResponseDto(    String orderId,    String userId,    String status,    Integer orderPrice,    Boolean isRefunded,    String address,    String paymentId,    String comment,    List<OrderItemResponseDto> itemList) {}
