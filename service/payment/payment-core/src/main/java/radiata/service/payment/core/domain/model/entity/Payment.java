357a0550b726d67b7ffa0440c22c91b8299dbe68
yunjae62 <wodbs7758@naver.com>
2024-10-02 22:01:30 +0900

    [#15] feat(payment): Payment 엔티티에 BaseEntity 추가

--- service/payment/payment-core/src/main/java/radiata/service/payment/core/domain/model/entity/Payment.java
+++ service/payment/payment-core/src/main/java/radiata/service/payment/core/domain/model/entity/Payment.java
@@ -13,6 +13,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLRestriction;
+import radiata.database.model.BaseEntity;
 import radiata.service.payment.core.domain.model.vo.Money;
 import radiata.service.payment.core.domain.model.vo.PaymentStatus;
 import radiata.service.payment.core.domain.model.vo.PaymentType;
@@ -24,7 +25,7 @@ import radiata.service.payment.core.domain.model.vo.PaymentType;
 @Builder // 빌더 패턴은 테스트 용도로만 사용
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 객체는 기본 생성자를 필요로 함
-public class Payment {
+public class Payment extends BaseEntity {
 
     @Id
     private String id;
