30d366515f55f75bf5769293d219b477e9769e7a
atto08 <atto08@naver.com>
2024-10-03 17:13:03 +0900

    [#14] feat(order): 엔티티와 Dto간 변환을 위해 OrderMapper 추가

---
+++ service/order/order-core/src/main/java/radiata/service/order/core/service/mapper/OrderMapper.java
@@ -0,0 +1,34 @@
+package radiata.service.order.core.service.mapper;
+
+import org.springframework.stereotype.Component;
+import radiata.common.domain.order.dto.request.OrderCreateRequestDto;
+import radiata.common.domain.order.dto.response.OrderResponseDto;
+import radiata.service.order.core.domain.model.entity.Order;
+
+@Component
+public class OrderMapper {
+
+    public Order toEntity(OrderCreateRequestDto requestDto, String orderId, String userId) {
+
+        return Order.of(
+            orderId,
+            userId,
+            requestDto.address(),
+            requestDto.comment()
+        );
+    }
+
+    public OrderResponseDto toDto(Order order) {
+
+        return OrderResponseDto.of(
+            order.getId(),
+            order.getUserId(),
+            order.getStatus().toString(),
+            order.getOrderPrice(),
+            order.getIsRefunded(),
+            order.getAddress(),
+            order.getPaymentId(),
+            order.getComment()
+            );
+    }
+}
