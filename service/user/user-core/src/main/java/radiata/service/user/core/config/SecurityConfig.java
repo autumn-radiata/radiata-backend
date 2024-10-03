7fa193d7d4cd88f13f87e6858836e1934d1d4173
SunMi <wywudi@naver.com>
2024-10-03 17:54:45 +0900

    [#16] feat(user) : security config추가

---
+++ service/user/user-core/src/main/java/radiata/service/user/core/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package radiata.service.user.core.config;
+
+
+import com.querydsl.core.annotations.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
