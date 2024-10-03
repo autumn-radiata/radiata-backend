5124ecf93b200513d042a1319e59bed504d52bd8
SunMi <wywudi@naver.com>
2024-10-03 17:52:39 +0900

    [#16] refactor(user) : 포인트 type 세분화

--- service/user/user-core/src/main/java/radiata/service/user/core/domain/model/constant/PointType.java
+++ service/user/user-core/src/main/java/radiata/service/user/core/domain/model/constant/PointType.java
@@ -2,7 +2,11 @@ package radiata.service.user.core.domain.model.constant;
 
 
 public enum PointType {
+    //사용 차감
     SUBSCRIBE,
-    INCREMENT;
+    //리뷰, 상품 증감
+    INCREMENT_REVIEW,
+    INCREMENT_PRODUCT,
+    ;
 
 }
