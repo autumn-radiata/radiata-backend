f6c60be3b64bf9e2a4ce297a8660e0b202b222f4
SunMi <wywudi@naver.com>
2024-10-06 21:22:10 +0900

    [#16] feat(user) : mapper추가

--- service/user/user-core/src/main/java/radiata/service/user/core/service/Mapper/UserMapper.java
+++ service/user/user-core/src/main/java/radiata/service/user/core/service/Mapper/UserMapper.java
@@ -4,14 +4,17 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import radiata.common.domain.user.dto.response.GetUserInfoResponseDto;
+import radiata.common.domain.user.dto.response.PointHistoryGetResponseDto;
+import radiata.service.user.core.domain.model.entity.PointHistory;
 import radiata.service.user.core.domain.model.entity.User;
 
 @Mapper(componentModel = SPRING)
 public interface UserMapper {
 
     @Mapping(source = "address.roadAddress", target = "roadAddress")
-    @Mapping(source="address.detailAddress",target="detailAddress")
-    @Mapping(source="address.zipcode",target="zipcode")
+    @Mapping(source = "address.detailAddress", target = "detailAddress")
+    @Mapping(source = "address.zipcode", target = "zipcode")
     GetUserInfoResponseDto userToGetUserInfoResponseDto(User user);
 
+    PointHistoryGetResponseDto userToPointHistoriesGetResponseDto(PointHistory history);
 }
