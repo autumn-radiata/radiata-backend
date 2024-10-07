622a4c45ecae56e435fcc9d405bcbea9347537c8
wonowonow <wonow_@naver.com>
2024-10-07 15:10:09 +0900

    [#27] feat(timesale): TimeSaleCreateRequestDto Validation 추가

--- common/src/main/java/radiata/common/domain/timesale/dto/request/TimeSaleCreateRequestDto.java
+++ common/src/main/java/radiata/common/domain/timesale/dto/request/TimeSaleCreateRequestDto.java
@@ -1,10 +1,14 @@
 package radiata.common.domain.timesale.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public record TimeSaleCreateRequestDto(
+    @NotBlank(message = "타임세일명을 입력해주세요.")
     String title,
+    @NotBlank(message = "타임세일명 시작일을 입력해주세요.")
     LocalDateTime timeSaleStartDate,
+    @NotBlank(message = "타임세일명 종료일을 입력해주세요.")
     LocalDateTime timeSaleEndDate
 ) {
 
