34bb633a4e4e43814a759e1929f3df80c5de03c3
atto08 <atto08@naver.com>
2024-10-07 21:19:22 +0900

    [#26] feat(order): 주문 성공 메시지 추가

--- common/src/main/java/radiata/common/message/SuccessMessage.java
+++ common/src/main/java/radiata/common/message/SuccessMessage.java
@@ -9,7 +9,15 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum SuccessMessage {
 
-    ;
+    /* 주문 */
+    CREATE_ORDER(HttpStatus.OK, "0", "주문 생성 완료"),
+    GET_ORDERS(HttpStatus.OK, "0", "주문 목록 조회 완료"),
+    GET_ORDER(HttpStatus.OK, "0", "주문 내역 조회 완료"),
+    COMPLETE_ORDER_PAYMENT(HttpStatus.OK, "0", "주문 결제 완료"),
+    UPDATE_ORDER(HttpStatus.OK, "0", "주문 상태 수정 완료"),
+    DELETE_ORDER(HttpStatus.OK, "0", "주문 내역 삭제 완료"),
+    CANCEL_ORDER(HttpStatus.OK, "0", "주문 취소 완료"),
+    REFUND_ORDER(HttpStatus.OK, "0", "주문 환불 완료");
 
     private final HttpStatus httpStatus;
     private final String code;
