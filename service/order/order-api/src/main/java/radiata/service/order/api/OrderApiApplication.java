e5fff581dcfd25b4bfb9f4e61b5e35f2c7cca54c
atto08 <atto08@naver.com>
2024-10-08 17:29:30 +0900

    [#26] refactor(order): jpaConfig 파일 추가

--- service/order/order-api/src/main/java/radiata/service/order/api/OrderApiApplication.java
+++ service/order/order-api/src/main/java/radiata/service/order/api/OrderApiApplication.java
@@ -2,10 +2,8 @@ package radiata.service.order.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = {"radiata.service.order"})
-@EnableJpaAuditing
 public class OrderApiApplication {
 
     public static void main(String[] args) {
