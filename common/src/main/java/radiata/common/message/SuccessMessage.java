0d001a9b643ff5463d2d6ff0381522a888dbafb1
atto08 <atto08@naver.com>
2024-10-08 12:46:39 +0900

    [#26] refactor(order): 성공 메시지 충돌 해결

--- common/src/main/java/radiata/common/message/SuccessMessage.java
+++ common/src/main/java/radiata/common/message/SuccessMessage.java
@@ -17,7 +17,8 @@ public enum SuccessMessage {
     UPDATE_ORDER(HttpStatus.OK, "0", "주문 상태 수정 완료"),
     DELETE_ORDER(HttpStatus.OK, "0", "주문 내역 삭제 완료"),
     CANCEL_ORDER(HttpStatus.OK, "0", "주문 취소 완료"),
-    REFUND_ORDER(HttpStatus.OK, "0", "주문 환불 완료");
+    REFUND_ORDER(HttpStatus.OK, "0", "주문 환불 완료"),
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;
