17a950edafa6574c93ac4dd9393a8f8651fddc46
atto08 <atto08@naver.com>
2024-10-10 00:35:52 +0900

    [#35] feat(order): 롤백 메서드(보상 트랜잭션) 추가

--- service/order/order-core/src/main/java/radiata/service/order/core/service/OrderItemService.java
+++ service/order/order-core/src/main/java/radiata/service/order/core/service/OrderItemService.java
@@ -1,14 +1,17 @@
 package radiata.service.order.core.service;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import radiata.common.annotation.Implementation;
 import radiata.common.domain.order.dto.response.OrderItemResponseDto;
 import radiata.service.order.core.domain.model.entity.OrderItem;
 import radiata.service.order.core.service.mapper.OrderItemMapper;
 
 @Implementation
+@Slf4j(topic = "OrderItemService")
 @RequiredArgsConstructor
 public class OrderItemService {
 
@@ -19,5 +22,25 @@ public class OrderItemService {
         return orderItems.stream()
             .map(orderItemMapper::toDto)
             .collect(Collectors.toSet());
+    }
+
+
+    // SAGA 보상 트랜잭션
+    public void rollbackTransaction(List<String> deductedProducts, List<String> usedCoupons) {
+
+        try {
+            // 상품 재고 - 보상
+            deductedProducts.forEach(productInfo -> {
+                // TODO - kafka 를 이용한 비동기 처리
+            });
+            // 쿠폰 상태 값 - 보상
+            usedCoupons.forEach(couponId -> {
+                // TODO - kafka 를 이용한 비동기 처리
+            });
+
+        } catch (Exception e) {
+            log.info("##########[보상 트랜잭션 처리 중, 예외 발생]##########");
+            throw new RuntimeException(e);
+        }
     }
 }
