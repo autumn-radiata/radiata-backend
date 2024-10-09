75ef0eb5785e39533f0328c76185b430c25427be
wonowonow <wonow_@naver.com>
2024-10-09 21:10:41 +0900

    [#39] feat(timesaleProduct): 타임세일 상품 Create Dto 및 인터페이스 디자인

---
+++ service/timesale/timesale-core/src/main/java/radiata/service/timesale/core/service/interfaces/TimeSaleProductCreateService.java
@@ -0,0 +1,9 @@
+package radiata.service.timesale.core.service.interfaces;
+
+import radiata.common.domain.timesale.dto.response.TimeSaleProductCreateDto;
+
+public interface TimeSaleProductCreateService {
+
+    void createTimeSaleProduct(TimeSaleProductCreateDto timeSaleProduct);
+
+}
