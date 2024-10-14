ebfbe315316cf8adbbaf5dba761f138ccdaf0f44
SunMi <wywudi@naver.com>
2024-10-15 00:35:23 +0900

    [#41] feat(brand) : 브랜드 서버 캐시,분산락 config

---
+++ service/brand/brand-core/src/main/java/radiata/service/brand/core/config/RedissonConfig.java
@@ -0,0 +1,31 @@
+package radiata.service.brand.core.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+
+public class RedissonConfig {
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private String port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+        config.useSingleServer().setPassword(password);
+        return Redisson.create(config);
+    }
+
+}
