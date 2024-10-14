959c4ce2a6e01c0bd54170cd6cfd47bd47be850e
atto08 <atto08@naver.com>
2024-10-14 20:19:36 +0900

    [#35] feat(order): 카프카 Config 파일 추가

---
+++ service/order/order-core/src/main/java/radiata/service/order/core/config/KafkaConfig.java
@@ -0,0 +1,9 @@
+package radiata.service.order.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// TODO - 정해지면 종류 별(결제, 타임세일 상품, 발급 쿠폰, 상품, 적립금(유저))로 등록
+public class KafkaConfig {
+
+}
