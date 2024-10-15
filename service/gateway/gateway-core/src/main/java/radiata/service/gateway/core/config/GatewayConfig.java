8207edf08a9a1f44feea6c5ee3eb0ecb2fdb674f
SunMi <wywudi@naver.com>
2024-10-15 14:07:29 +0900

    [#41] refactor(gateway) : filter 각 route에 추가 설정

--- service/gateway/gateway-core/src/main/java/radiata/service/gateway/core/config/GatewayConfig.java
+++ service/gateway/gateway-core/src/main/java/radiata/service/gateway/core/config/GatewayConfig.java
@@ -1,6 +1,7 @@
 package radiata.service.gateway.core.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,20 +14,21 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-            .route("user-service", r -> r.path("/auth/**", "user/**")
-                .filters(f -> f
-                    .circuitBreaker(c -> c
-                        .setName("user-service")
-                        .setFallbackUri("forward:/fallBack")
-                    )
-                    .retry(c -> c
-                        .setRetries(3)
-                        .setSeries(HttpStatus.Series.SERVER_ERROR)
-                        .setMethods(HttpMethod.GET))
-                )
-                .uri("lb://user-service")
-            )
+            .route("user-service", r -> r.path("/auth/**", "users/**")
+                .filters(f -> applyCommonFilters(f, "user-service"))
+                .uri("lb://user-service"))
             .build();
+    }
+
+    private GatewayFilterSpec applyCommonFilters(GatewayFilterSpec f, String serviceName) {
+        return f.circuitBreaker(c -> c
+                .setName(serviceName)
+                .setFallbackUri("forward:/fallBack")
+            )
+            .retry(c -> c
+                .setRetries(3)
+                .setSeries(HttpStatus.Series.SERVER_ERROR)
+                .setMethods(HttpMethod.GET));
     }
 
 }
