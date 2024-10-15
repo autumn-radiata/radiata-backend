2465943c93efa2a18aba311a0b3df2d9423066b4
atto08 <atto08@naver.com>
2024-10-15 15:38:06 +0900

    [#67] refactor(order): 주문 Core Config 설정 수정

--- service/order/order-api/src/main/java/radiata/service/order/api/OrderApplication.java
+++ service/order/order-api/src/main/java/radiata/service/order/api/OrderApplication.java
@@ -2,8 +2,10 @@ package radiata.service.order.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication(scanBasePackages = {"radiata.service.order"})
+@SpringBootApplication(scanBasePackages = {"radiata.service.order", "radiata.database", "radiata.common"})
+@ConfigurationPropertiesScan(basePackages = {"radiata.service.order", "radiata.database", "radiata.common"})
 public class OrderApplication {
 
     public static void main(String[] args) {
