5a60a6f245248630357c14f0c4ac89ad16303f0d
atto08 <atto08@naver.com>
2024-10-17 01:21:48 +0900

    [#83] feat(order): get 쿠폰 정보 메서드 추가

--- service/order/order-core/src/main/java/radiata/service/order/core/service/client/CouponIssueClient.java
+++ service/order/order-core/src/main/java/radiata/service/order/core/service/client/CouponIssueClient.java
@@ -1,10 +1,12 @@
 package radiata.service.order.core.service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import radiata.common.domain.coupon.dto.response.CouponIssueResponseDto;
+import radiata.common.domain.coupon.dto.response.CouponResponseDto;
 import radiata.common.response.SuccessResponse;
 
 @FeignClient(name = "coupon-service")
@@ -13,4 +15,7 @@ public interface CouponIssueClient {
     @PatchMapping("/couponissues/{couponIssueId}")
     SuccessResponse<CouponIssueResponseDto> useCouponIssue(@PathVariable String couponIssueId,
         @RequestHeader("X-UserId") String userId);
+
+    @GetMapping("/coupons/{couponId}")
+    SuccessResponse<CouponResponseDto> getCouponType(@PathVariable String couponId);
 }
