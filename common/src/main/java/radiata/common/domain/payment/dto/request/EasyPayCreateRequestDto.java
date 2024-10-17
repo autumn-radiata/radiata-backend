74aa0c67837cdc9f358ccbfa868a149c94212be8
yunjae62 <wodbs7758@naver.com>
2024-10-17 14:34:06 +0900

    [#86] fix(payment): 검증 로직 수정

--- common/src/main/java/radiata/common/domain/payment/dto/request/EasyPayCreateRequestDto.java
+++ common/src/main/java/radiata/common/domain/payment/dto/request/EasyPayCreateRequestDto.java
@@ -2,13 +2,14 @@ package radiata.common.domain.payment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record EasyPayCreateRequestDto(
 
     @NotBlank(message = "userId는 필수입니다.")
     String userId,
 
-    @NotBlank(message = "orderId는 필수입니다.")
+    @PositiveOrZero(message = "amount는 0 이상이어야 합니다.")
     Long amount,
 
     @Pattern(regexp = "^[0-9]{6}$", message = "password는 6자리 숫자여야 합니다.")
