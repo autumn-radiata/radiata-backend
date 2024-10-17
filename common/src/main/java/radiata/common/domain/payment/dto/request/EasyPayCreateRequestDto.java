2c876372b64b683616601d1036c88078780a6888
yunjae62 <wodbs7758@naver.com>
2024-10-17 14:24:17 +0900

    [#86] feat(payment): 간편결제 DTO 요청 시 검증 추가

--- common/src/main/java/radiata/common/domain/payment/dto/request/EasyPayCreateRequestDto.java
+++ common/src/main/java/radiata/common/domain/payment/dto/request/EasyPayCreateRequestDto.java
@@ -1,8 +1,17 @@
 package radiata.common.domain.payment.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record EasyPayCreateRequestDto(
+
+    @NotBlank(message = "userId는 필수입니다.")
     String userId,
+
+    @NotBlank(message = "orderId는 필수입니다.")
     Long amount,
+
+    @Pattern(regexp = "^[0-9]{6}$", message = "password는 6자리 숫자여야 합니다.")
     String password
 ) {
 
