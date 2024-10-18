96732dbc507ac0085f699df0d4d67c6c91da5505
atto08 <atto08@naver.com>
2024-10-19 03:25:00 +0900

    [#80] feat(order): 결제취소 비동기 요청 추가(Kafka)

--- service/order/order-core/src/main/java/radiata/service/order/core/config/KafkaConfig.java
+++ service/order/order-core/src/main/java/radiata/service/order/core/config/KafkaConfig.java
@@ -1,9 +1,32 @@
 package radiata.service.order.core.config;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 // TODO - 정해지면 종류 별(결제, 타임세일 상품, 발급 쿠폰, 상품, 적립금(유저))로 등록
 public class KafkaConfig {
 
+    @Bean
+    public ProducerFactory<String, String> paymentRollbackProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        // TODO - 포트 주소 "yunjae.click"으로 수정 예정
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> paymentRollbackKafkaTemplate() {
+        return new KafkaTemplate<>(paymentRollbackProducerFactory());
+    }
 }
--- service/order/order-core/src/main/java/radiata/service/order/core/service/RollbackService.java
+++ service/order/order-core/src/main/java/radiata/service/order/core/service/RollbackService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import radiata.common.domain.order.dto.response.StockInfoDto;
 import radiata.service.order.core.domain.model.entity.OrderItem;
 import radiata.service.order.core.service.context.OrderRollbackContext;
 
@@ -13,13 +15,16 @@ import radiata.service.order.core.service.context.OrderRollbackContext;
 @RequiredArgsConstructor
 public class RollbackService {
 
+    private final KafkaTemplate<String, String> paymentKafkaTemplate;
+
     // 보상 트랜잭션 - 타임 세일 상품 재고
-    private void rollbackTimeSaleStock(List<String> deductedTimeSaleStocks) {
+    private void rollbackTimeSaleStock(List<StockInfoDto> deductedTimeSaleStocks) {
         try {
             // 타임세일 상품 재고 증감 요청
             deductedTimeSaleStocks.forEach(timeSaleProduct -> {
                 // TODO - kafka 를 이용한 비동기 처리
-                System.out.println("timeSaleProductId = " + timeSaleProduct);
+                System.out.println("timeSaleProductId = " + timeSaleProduct.id());
+                System.out.println("timeSale quantity = " + timeSaleProduct.quantity());
             });
         } catch (RuntimeException e) {
             log.error(" [Rollback Error]: TimeSale-Service ");
@@ -27,12 +32,13 @@ public class RollbackService {
     }
 
     // 보상 트랜잭션 - 상품 재고
-    private void rollbackProductStock(List<String> deductedProductStocks) {
+    private void rollbackProductStock(List<StockInfoDto> deductedProductStocks) {
         try {
             // 상품 재고 증감 요청
             deductedProductStocks.forEach(product -> {
                 // TODO - kafka 를 이용한 비동기 처리
-                System.out.println("productId = " + product);
+                System.out.println("productId = " + product.id());
+                System.out.println("product quantity = " + product.quantity());
             });
         } catch (RuntimeException e) {
             log.error(" [Rollback Error]: Product-Service ");
@@ -65,7 +71,8 @@ public class RollbackService {
     private void rollbackPaymentAmount(String paymentId) {
         try {
             // 결제 취소 요청 - (== 환불)
-
+            // TODO - userId 필요할까?
+            paymentKafkaTemplate.send("paymentRollback", paymentId);
         } catch (RuntimeException e) {
             log.error(" [Rollback Error]: Payment-Service ");
         }
@@ -75,6 +82,7 @@ public class RollbackService {
     // 주문 등록 - 보상 트랜잭션
     public void createOrderItemsRollbackTransaction(OrderRollbackContext context) {
 
+        List<StockInfoDto> test = context.getDeductedProducts();
         rollbackTimeSaleStock(context.getDeductedTimeSales());
         rollbackProductStock(context.getDeductedProducts());
         rollbackCoupons(context.getUsedCoupons());
