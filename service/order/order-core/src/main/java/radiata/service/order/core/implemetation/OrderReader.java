9aade1d1e116f11c92f3237ac7c598de2309039d
atto08 <atto08@naver.com>
2024-10-22 16:43:17 +0900

    [#90] feat(order): 주문 자동취소 스케줄러 추가

--- service/order/order-core/src/main/java/radiata/service/order/core/implemetation/OrderReader.java
+++ service/order/order-core/src/main/java/radiata/service/order/core/implemetation/OrderReader.java
@@ -2,11 +2,13 @@ package radiata.service.order.core.implemetation;
 
 import static radiata.common.message.ExceptionMessage.NOT_FOUND;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import radiata.common.annotation.Implementation;
 import radiata.common.exception.BusinessException;
 import radiata.service.order.core.domain.model.entity.Order;
+import radiata.service.order.core.domain.repository.OrderQueryRepository;
 import radiata.service.order.core.domain.repository.OrderRepository;
 
 @Implementation
@@ -14,11 +16,19 @@ import radiata.service.order.core.domain.repository.OrderRepository;
 public class OrderReader {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @Transactional(readOnly = true)
     public Order readOrder(String orderId) {
 
         return orderRepository.findById(orderId)
             .orElseThrow(() -> new BusinessException(NOT_FOUND));
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<Order> readOrders() {
+
+        return orderQueryRepository.findOrdersNotCompletedWithin10Minutes();
     }
 }
