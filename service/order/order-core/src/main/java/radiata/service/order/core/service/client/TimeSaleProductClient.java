6be817ddaca371c2382c6cae0dab2247bf1ee8da
atto08 <atto08@naver.com>
2024-10-24 01:46:54 +0900

    [#96] refactor(order): 타임세일 상품 할인율 가져오기 위한 타입 변경

--- service/order/order-core/src/main/java/radiata/service/order/core/service/client/TimeSaleProductClient.java
+++ service/order/order-core/src/main/java/radiata/service/order/core/service/client/TimeSaleProductClient.java
@@ -3,11 +3,15 @@ package radiata.service.order.core.service.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import radiata.common.domain.timesale.dto.request.TimeSaleProductSaleRequestDto;
+import radiata.common.domain.timesale.dto.response.TimeSaleProductResponseDto;
 import radiata.common.response.SuccessResponse;
 
 @FeignClient(name = "timesale-service")
 public interface TimeSaleProductClient {
 
     @PatchMapping("/timesale-products/{timeSaleProductId}")
-    SuccessResponse<String> checkAndDeductTimeSaleProduct(@PathVariable String timeSaleProductId);
+    SuccessResponse<TimeSaleProductResponseDto> checkAndDeductTimeSaleProduct(@PathVariable String timeSaleProductId,
+        @RequestBody TimeSaleProductSaleRequestDto requestDto);
 }
