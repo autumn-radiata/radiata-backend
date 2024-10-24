a767b0091e1e345a1ed43cab7d624cfa6b3bd22e
SunMi <wywudi@naver.com>
2024-10-24 15:13:50 +0900

    [] fix(user) : 보상 트랜잭션 복구 수정

---
+++ service/user/user-core/src/main/java/radiata/service/user/core/service/UserCommandService.java
@@ -0,0 +1,83 @@
+package radiata.service.user.core.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import radiata.common.domain.user.dto.request.PointModifyRequestDto;
+import radiata.common.domain.user.dto.request.UserModifyRequestDto;
+import radiata.common.exception.BusinessException;
+import radiata.common.message.ExceptionMessage;
+import radiata.service.user.core.domain.model.constant.PointType;
+import radiata.service.user.core.domain.model.entity.User;
+import radiata.service.user.core.domain.repository.PointHistoryRepository;
+import radiata.service.user.core.domain.repository.UserRepository;
+import radiata.service.user.core.implement.PointHandler;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserCommandService {
+
+    private final UserRepository userRepository;
+    private final PointHandler pointHandler;
+    private final PointHistoryRepository pointHistoryRepository;
+
+    /**
+     * 회원 정보 수정
+     */
+    @Transactional
+    public void updateUserInfo(String userId, UserModifyRequestDto dto) {
+        User user = findValidUser(userId);
+        user.updateInfo(
+            dto.nickname(),
+            dto.phone(),
+            dto.roadAddress(),
+            dto.detailAddress(),
+            dto.zipcode()
+        );
+    }
+
+    /**
+     * 회원 삭제
+     */
+    @Transactional
+    public void removeUser(String userId) {
+        User user = findValidUser(userId);
+        user.delete(userId);
+    }
+
+    /**
+     * 포인트 차감
+     */
+    @Transactional
+    public void deductPoint(PointModifyRequestDto dto) {
+        User user = findValidUser(dto.userId());
+        user.deductPoint(dto.point());
+        pointHandler.recordPointHistory(user, dto.point(), PointType.SUBSCRIBE);
+    }
+
+    /**
+     * 포인트 증감 -보상 트랜잭션 복구
+     */
+    @Transactional
+    @KafkaListener(topics = "user.add-point", containerFactory = "KafkaListenerContainerFactory")
+    public void increasePoint(ConsumerRecord<String, String> consumerRecord) {
+        PointModifyRequestDto stockAddRequestDto = EventSerializer.deserialize(consumerRecord.value(),
+            PointModifyRequestDto.class);
+        User user = findValidUser(stockAddRequestDto.userId());
+        user.addPoint(stockAddRequestDto.point());
+    }
+
+    /**
+     * 포인트 증감 - 리뷰 정산
+     */
+
+    private User findValidUser(String userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new BusinessException(ExceptionMessage.USER_NOT_FOUND));
+    }
+
+}
